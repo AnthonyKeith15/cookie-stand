b94f4aabaca3e2f0a6fd6cf47cb7034443f27d1e
Anthony Keith <jt1keith@gmail.com>
2023-01-31 11:39:34 -0800

    moved constructor function to top, added functions to franchiseLocation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
+function franchiseLocation(location, minCust, maxCust, aveOrderSize) {
+  this.storeLocation = location;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.aveOrderSize = aveOrderSize;
+  this.hours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
+  this.salesByHour = [];
+  this.calcRandNumOfCust = function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+  };
+  this.calcCookiesSold = function() {
+    return Math.floor(this.calcRandNumOfCust() * this.aveOrderSize);
+  };
+  this.calcHourlySales = function() {
+    for (let i = 0; i < this.hours.length; i++) {
+      this.salesByHour.push(this.calcCookiesSold());
+    }
+    return this.salesByHour;
+  };
+  this.calcSumOfHourlySales = function () {
+    let runningTotal = 0;
+    for (let i = 0; i < this.salesByHour.length; i++) {
+      runningTotal += this.salesByHour[i];
+    }
+    return runningTotal;
+  };
+}
+
 let seattleStore = {
   location: 'Seattle',
   hours: ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'],
@@ -26,33 +54,6 @@ let seattleStore = {
   }
 };
 
-function franchiseLocation(location, minCust, maxCust, aveOrderSize) {
-  this.storeLocation = location;
-  this.minCust = minCust;
-  this.maxCust = maxCust;
-  this.aveOrderSize = aveOrderSize;
-  this.hours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
-  this.salesByHour = [];
-  this.calcRandNumOfCust = function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-  };
-  this.calcCookiesSold = function() {
-    return Math.floor(this.calcRandNumOfCust() * this.aveOrderSize);
-  };
-  this.calcHourlySales = function() {
-    for (let i = 0; i < this.hours.length; i++) {
-      this.salesByHour.push(this.calcCookiesSold());
-    }
-    return this.salesByHour;
-  };
-  this.calcSumOfHourlySales = function () {
-    let runningTotal = 0;
-    for (let i = 0; i < this.salesByHour.length; i++) {
-      runningTotal += this.salesByHour[i];
-    }
-    return runningTotal;
-  }
-}
 
 let tokyoStore = {
   location: 'Tokyo',
@@ -201,6 +202,15 @@ function generateSalesReport(storeLocation) {
 let salesList = document.getElementById('sales');
 
 let storeArr = [seattleStore, tokyoStore, dubaiStore, parisStore, limaStore];
+
+let seattleFranchiseLocation = new franchiseLocation (
+  'Seattle',
+  2,
+  9,
+  4
+)
+console.log(seattleFranchiseLocation.storeLocation);
+console.log(seattleFranchiseLocation.calcRandNumOfCust());
 
 /*for (let k = 0; k < storeArr.length; k++){
   generateSalesReport(storeArr[k]);
