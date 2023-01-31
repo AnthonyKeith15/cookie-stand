004b75e63d743214551af0ef8c11f64b76205b79
Anthony Keith <jt1keith@gmail.com>
2023-01-31 11:32:11 -0800

    created constructor function for franchise locations

--- js/app.js
+++ js/app.js
@@ -26,6 +26,34 @@ let seattleStore = {
   }
 };
 
+function franchiseLocation(location, minCust, maxCust, aveOrderSize) {
+  this.storeLocation = location;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.aveOrderSize = aveOrderSize;
+  this.hours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
+  this.salesByHour = [];
+  this.calcRandNumOfCust = function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+  };
+  this.calcCookiesSold = function() {
+    return Math.floor(this.calcRandNumOfCust() * this.aveOrderSize);
+  };
+  this.calcHourlySales = function() {
+    for (let i = 0; i < this.hours.length; i++) {
+      this.salesByHour.push(this.calcCookiesSold());
+    }
+    return this.salesByHour;
+  };
+  this.calcSumOfHourlySales = function () {
+    let runningTotal = 0;
+    for (let i = 0; i < this.salesByHour.length; i++) {
+      runningTotal += this.salesByHour[i];
+    }
+    return runningTotal;
+  }
+}
+
 let tokyoStore = {
   location: 'Tokyo',
   hours: ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'],
@@ -174,7 +202,7 @@ let salesList = document.getElementById('sales');
 
 let storeArr = [seattleStore, tokyoStore, dubaiStore, parisStore, limaStore];
 
-for (let k = 0; k < storeArr.length; k++){
+/*for (let k = 0; k < storeArr.length; k++){
   generateSalesReport(storeArr[k]);
 }
-
+*/
