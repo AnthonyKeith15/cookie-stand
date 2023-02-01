d732a75df5f257ddf7b235fb767d052625c23819
Anthony Keith <jt1keith@gmail.com>
2023-02-01 11:29:32 -0800

    full table works as intended

--- js/app.js
+++ js/app.js
@@ -57,19 +57,95 @@ function franchiseLocation(location, minCust, maxCust, aveOrderSize) {
 };
 // global variable hours
 let hours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
-
-
-
+// All object instances initialized here
 let seattleFranchiseLocation = new franchiseLocation('Seattle', 23, 65, 6.3);
 let tokyoFranchiseLocation = new franchiseLocation('Tokyo', 3, 24, 1.2);
 let dubaiFranchiseLocation = new franchiseLocation('Dubai', 11, 38, 3.7);
 let parisFranchiseLocation = new franchiseLocation('Paris', 20, 38, 2.3);
 let limaFranchiseLocation = new franchiseLocation('Lima', 2, 16, 4.6);
 
+function createHeading(){
+  // Selects the html element table
+  let salesTable = document.querySelector('table');
+  // Creates a table heading element, saves it as variable tHead
+  let tHead = document.createElement('thead');
+  //Append tHead to the table
+  salesTable.appendChild(tHead);
+  // Create a table row element save it as variable headingRow
+  let headingRow = document.createElement('tr');
+  // Append headingRow to tHead element
+  tHead.appendChild(headingRow);
+  // Create a table header element, save it as hElement
+  let hElement = document.createElement('th');
+  // Set Textcontent of first hElement to 'Location'
+  hElement.textContent = 'Location';
+  // Append hElement to headingRow
+  headingRow.appendChild(hElement);
+  //Create for loop, loop through hours array and create a new hElement for each Hour
+  for (let i = 0; i < hours.length; i++){
+    let hourElement = document.createElement('th');
+    hourElement.textContent = hours[i];
+    headingRow.appendChild(hourElement);
+  }
+  //OUTSIDE THE LOOP Create new hElement
+  let lastElement = document.createElement('th');
+  // Set text content
+  lastElement.textContent = 'Total Daily Sales';
+  // Append Child
+  headingRow.appendChild(lastElement);
+}
+
+function createTableBody() {
+  for (let i = 0; i < allFranchisesArr.length; i++){
+    allFranchisesArr[i].calcHourlySales();
+    allFranchisesArr[i].renderTable();
+  }
+}
+
+function createFooter() {
+  // Selects the HTML element table
+  let salesTable = document.querySelector('table');
+  // Creates a table footer element, saves it as variable tFoot
+  let tFoot = document.createElement('tfoot');
+  // Appends tFoot to the table
+  salesTable.appendChild(tFoot);
+  // Creates a table row element, saves it as variable footerRow
+  let footerRow = document.createElement('tr');
+  // Append footerRow to tFoot element
+  tFoot.appendChild(footerRow);
+  // Create a table header element, save it as footLabel
+  let footLabel = document.createElement('th');
+  // Set textcontent of first footLabel to 'Total Hourly Sales'
+  footLabel.textContent = 'Totals';
+  // Append footLabel to footerRow
+  footerRow.appendChild(footLabel);
+  let sales = allFranchisesArr;
+  //Create foor loop that loops through sales[j], then salesByHour[i]. Using hours.length so it loops through for every hour
+  for (let i = 0; i < hours.length; i++){
+    let runningTotal = 0;
+    for (let j = 0; j < sales.length; j++){
+      runningTotal += sales[j].salesByHour[i];
+    }
+    // Create table data element, saves it as variable totalByHour
+    let totalByHour = document.createElement('td');
+    // Set the text content of totalByHour to runningTotal
+    totalByHour.textContent = runningTotal;
+    //Append Total by hour to footerRow
+    footerRow.appendChild(totalByHour);
+  }
+  //Calculate Total for All Locations
+  let sumOfAllStores = 0
+  for (let i = 0; i < allFranchisesArr.length; i++) {
+    sumOfAllStores += allFranchisesArr[i].calcSumOfHourlySales();
+  }
+  //Create a new table data element, save it as allStoresSales
+  let allStoresSales = document.createElement('th');
+  allStoresSales.textContent = sumOfAllStores;
+  footerRow.appendChild(allStoresSales);
+}
 
 let allFranchisesArr = [seattleFranchiseLocation, tokyoFranchiseLocation, dubaiFranchiseLocation, parisFranchiseLocation, limaFranchiseLocation];
 
-for (let i = 0; i < allFranchisesArr.length; i++){
-  allFranchisesArr[i].calcHourlySales();
-  allFranchisesArr[i].renderTable();
-}
+createHeading();
+createTableBody();
+createFooter();
